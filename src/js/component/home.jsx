c3606702ab7f9d026fd07a98ed71512d0305b1de
Alvaro <curielyseguro@gmail.com>
2023-11-13 15:57:58 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+// TodoList.jsx
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+  const addTask = () => {
+    if (taskInput.trim() !== '') { 
+      setTasks([...tasks, taskInput]); 
+      setTaskInput(''); 
+    }
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index)); 
+  };
+
+  return (
+    <div id="todo-list">
+      <h1>Lista de Tareas</h1>
+      <input
+        type="text"
+        placeholder="Agregar nueva tarea"
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+        onKeyUp={(e) => e.key === 'Enter' && addTask()}
+      />
+      <ul id="task-list">
+        {tasks.length === 0 ? (
+          <p id="no-tasks">Sin tareas, agrega más tareas...</p>
+        ) : (
+          tasks.map((task, index) => (
+            <li key={index}>
+              <span>{task}</span>
+              <span className="delete-task" onClick={() => deleteTask(index)}>
+			  ✖️
+              </span>
+            </li>
+          ))
+        )}
+      </ul>
+      {tasks.length > 0 && ( 
+        <div className="item-count">
+          {tasks.length === 1 ? `${tasks.length} tarea pendiente` : `${tasks.length} tareas pendientes`}
+        </div>
+      )}
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
